6f97e535b583cf227296635a53e8907b593b7ffe
Amreal-Lamanera <pieraccini.francesco@gmail.com>
2022-08-03 14:33:29 +0200

    milestone 1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // console.log('js ok');
 
+// creo un array e lo riempio di oggetti
 const ourTeam = new Array;
 ourTeam.push(getObject('Wayne Bernett', 'Founder & CEO', 'wayne-barnett-founder-ceo.jpg'));
 ourTeam.push(getObject('Angela Caroll', 'Chief Editor', 'angela-caroll-chief-editor.jpg'));
@@ -7,9 +8,22 @@ ourTeam.push(getObject('Walter Gordon', 'Office Manager	', 'walter-gordon-office
 ourTeam.push(getObject('Angela Lopez', 'Social Media Manager', 'angela-lopez-social-media-manager.jpg'));
 ourTeam.push(getObject('Scott Estrada', 'Developer', 'scott-estrada-developer.jpg'));
 ourTeam.push(getObject('WBarbara Ramosayne', 'Graphic Designer', 'barbara-ramos-graphic-designer.jpg'));
+// console.log(ourTeam);
 
-console.log(ourTeam);
+// stampo in console le info
+ourTeam.forEach(consolePrint);
 
+/**********************************************************
+    Funzione che stampa in console un oggetto.
+**********************************************************/
+function consolePrint(element) {
+    console.log(`Nome e Cognome: ${element.name}, Ruolo: ${element.role}, Immagine ${element.img}`);
+}
+
+/**********************************************************
+    Funzione che crea un oggetto con i dati forniti e lo
+    ritorna.
+**********************************************************/
 function getObject(name, role, img) {
     const obj = new Object;
     obj.name = name;
